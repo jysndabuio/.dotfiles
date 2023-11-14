59469cbcd4becd6981f7e3a74cb16d0cf6e0a97b
jysndabuio <jysndabu@outlook.com>
2023-11-14 18:32:27 +0100

    added symlink code for zshrc

--- install_system.sh
+++ install_system.sh
@@ -1,3 +1,6 @@
 sudo pacman install curl zsh git
 git config --global user.name 'jysndabuio'
 git config --global user.email 'jysndabu@outlook.com'
+cp ln .sf ~/.zshrc ~/.zshrc.bak
+rm ~/.zshrc
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
